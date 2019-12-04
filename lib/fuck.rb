33b80502b22a1c4fe8a48b71768c317998609fa0
Patrick Colina <trick.co@yahoo.com>
2019-12-04 03:38:37 +0000

    Automatically backed up by Learn

--- lib/fuck.rb
+++ lib/fuck.rb
@@ -1,5 +1,24 @@
 def say_fuck_repeatedly
+  
   phrase = "fuck"
-
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
+  phrase = "fuck"
 
 end
